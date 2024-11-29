4906192c0793e3c54e8a46ba52ce896d33c5e773
Nanthawat Shinakham <nanthawatshinakham@gmail.com>
2024-11-29 17:39:39 +0700

    17.40test

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,31 @@
+<div class="bg-[] w-[100%] h-[12vh] space-y-8 Navbar-main">
+    <div class="w-[84%] m-auto topbar-box">
+        <div class="topbar">
+            <ul class="space-x-4 text-sm font-[460] flex">
+                <li>bchcustomerconnect@bdms.co.th </li>
+                <li>039319888</li>
+                <li>25/14 Thaluang Watmai Muang Chanthaburi</li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="w-[82%] m-auto navbar-box">
+        <div class="flex justify-between items-center navbar">
+            <ul class="space-x-12 text-[#223851] text-base font-[530] flex items-center">
+                <li class="hover:text-[#1760c0] transition cursor-pointer">Home</li>
+                <li class="hover:text-[#1760c0] transition cursor-pointer">About Us</li>
+                <li class="hover:text-[#1760c0] transition cursor-pointer">Customer Service</li>
+                <li class="hover:text-[#1760c0] transition cursor-pointer">Health Insurance</li>
+                <li class="font-bold hover:text-[#1760c0] transition cursor-pointer">Package and Promotions</li>
+                <li class="hover:text-[#1760c0] transition cursor-pointer">Contact</li>
+                <li class="hover:text-[#1760c0] transition cursor-pointer">Member</li>
+            </ul>
+
+            <ul class="space-x-2 flex">
+                <li class="bg-[#031e61] text-[white] font-[530] p-2 px-6 rounded cursor-pointer">Appointment</li>
+                <li class="bg-[#031e61] text-[white] font-[530] p-2 px-6 rounded cursor-pointer">EN</li>
+            </ul>
+        </div>
+    </div>
+    
+</div>
