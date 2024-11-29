1e5db475ccac13e89a59745f87e530402a35a3d8
rainy <rainydayisbright@gmail.com>
2024-11-29 20:19:11 +0700

    send footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,51 @@
+<div class="footer-top">
+    <div>
+        <h5>โรงพยาบาลกรุงเทพจันทบุรี</h5>
+        <p>
+            25/14 Tharoung road vadmhai<br>
+            Mueng Chanthaburi<br><br>
+            <strong>tel:</strong> 039-319-888<br>
+            <strong>Email:</strong> bchhr@bch.co.th<br>
+        </p>
+    </div>
+    <div>
+        <h4>Useful links</h4>
+        <ul>
+            <li>
+                <i></i>
+                <a href="#">Homepage</a>
+            </li>
+            <li>
+                <i></i>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <i></i>
+                <a href="#">Services</a>
+            </li>
+            <li>
+                <i></i>
+                <a href="#">Terms of service</a>
+            </li>
+            <li>
+                <i></i>
+                <a href="#">Personal information policy</a>
+            </li>
+        </ul>
+    </div>
+    <div>
+        <h4>Our services</h4>
+        <ul>
+            <li><a href="#">medical services</a></li>
+            <li><a href="#">Apply for a job with us</a></li>
+            <li><a href="#" class="text-blue-500">Make a doctor's appointment</a></li>
+        </ul>
+    </div>
+</div>
+<div class="footer-buttom">
+    <div>
+        <div>
+
+        </div>
+    </div>
+</div>
